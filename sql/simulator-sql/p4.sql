87cfbd23b852797df9c699202a1a3ec7baad38b6
Alexey Pronkin <mio.alexey@gmail.com>
2023-01-10 02:57:36 +0000

    add 8 problem

--- sql/simulator-sql/p4.sql
+++ sql/simulator-sql/p4.sql
@@ -106,3 +106,20 @@ FROM orders
 GROUP BY order_size
 HAVING COUNT(order_id) > 5000
 ORDER BY order_size ASC
+
+
+-- Задание:
+
+-- Из таблицы courier_actions отберите id трёх курьеров, доставивших в сентябре 2022 года наибольшее количество заказов.
+-- Выведите две колонки — id курьера и число доставленных заказов.
+-- Колонку с числом доставленных заказов назовите delivered_orders.
+-- Пояснение:
+-- Помните, что в таблице courier_actions есть информация как о созданных, так и о доставленных заказах.
+-- Поля в результирующей таблице: courier_id, delivered_orders
+
+SELECT courier_id, COUNT(DISTINCT order_id) as delivered_orders
+FROM courier_actions
+WHERE action='deliver_order' AND DATE_PART('year', time)='2022' AND DATE_PART('month', time)='9'
+GROUP BY courier_id
+ORDER BY delivered_orders DESC
+LIMIT 3
