6486f9b95b672b2c9c1df6ccbaaa095619ccd769
Alexey Pronkin <mio.alexey@gmail.com>
2023-01-10 02:57:02 +0000

    add 8 problem

--- sql/simulator-sql/p4.sql
+++ sql/simulator-sql/p4.sql
@@ -84,3 +84,25 @@ SELECT array_length(product_ids, 1) as order_size, COUNT(order_id) as orders_cou
 FROM orders
 GROUP BY order_size
 ORDER BY order_size ASC
+
+-- Задание:
+
+-- Посчитайте количество товаров в каждом заказе из таблицы orders, примените к этим значениям группировку и посчитайте количество заказов в каждой группе. 
+-- Выведите две колонки: количество товаров в заказе и число заказов с таким количеством. 
+-- Колонки назовите соответственно order_size и orders_count. 
+-- Результат отсортируйте по возрастанию числа товаров в заказе.
+
+-- Поля в результирующей таблице: order_size, orders_count
+
+
+-- Дополните предыдущий запрос оператором HAVING и отберите только те размеры заказов, общее число которых превышает 5000. 
+-- Вновь выведите две колонки: количество товаров в заказе и число заказов с таким количеством. 
+-- Колонки назовите соответственно order_size и orders_count. Результат отсортируйте по возрастанию числа товаров в заказе.
+
+-- Поля в результирующей таблице: order_size, orders_count
+
+SELECT array_length(product_ids, 1) as order_size, COUNT(order_id) as orders_count
+FROM orders
+GROUP BY order_size
+HAVING COUNT(order_id) > 5000
+ORDER BY order_size ASC
